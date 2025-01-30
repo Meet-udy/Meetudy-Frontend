9a2ba4f13f5e5386c02a0c5284268c9cfeb938e6
hershyYJ <juj0478@naver.com>
2025-01-30 16:07:43 +0900

    [feature/#3] Add routing for login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
-import { HomePage } from "./pages/HomePage.tsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HomePage } from "./pages/HomePage/HomePage.tsx";
+import LoginPage from "./pages/LoginPage/LoginPage.tsx"; 
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} /> 
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
