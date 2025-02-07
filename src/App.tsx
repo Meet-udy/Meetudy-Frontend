0ba830dab4b21c0c9962665cba7facc4cd32c1a4
hershyYJ <juj0478@naver.com>
2025-02-07 17:09:17 +0900

    [refactor/#5] Update import paths due to package structure changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { HomePage } from "./pages/HomePage/HomePage.tsx";
-import LoginPage from "./pages/LoginPage/LoginPage.tsx"; 
+import { HomePage } from "./pages/Home/HomePage.tsx";
+import LoginPage from "./pages/Auth/LoginPage.tsx"; 
 
 function App() {
   return (
