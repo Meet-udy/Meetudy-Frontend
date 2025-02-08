345cdff1f93673cc4054632f6636455d0149765f
hershyYJ <juj0478@naver.com>
2025-02-08 14:17:53 +0900

    [feature/#9] Add routing for Kakao login redirect URI

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/Home/HomePage.tsx";
 import LoginPage from "./pages/Auth/LoginPage.tsx"; 
+import KakaoRedirectHandler from "./pages/Auth/KakaoRedirectHandler.tsx";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} /> 
+          <Route path="/oauth2/callback/kakao" element={<KakaoRedirectHandler />} />
         </Routes>
       </div>
     </Router>
