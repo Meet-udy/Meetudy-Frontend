fe78122d9bbff0561bcba5dadb9e198cc93fdf0b
hershyYJ <juj0478@naver.com>
2025-02-23 17:03:32 +0900

    [refactor/#15] Extract locationMapping and categoryMapping into constants

---
+++ src/constants/categoryMapping.ts
@@ -0,0 +1,13 @@
+export const categoryMapping: { [key: string]: string } = {
+  LANGUAGE: "어학",
+  CERTIFICATION: "자격증",
+  HUMANITIES: "인문학",
+  SOCIAL_SCIENCES: "사회과학",
+  DESIGN: "디자인",
+  SCIENCE: "과학",
+  PROGRAMMING: "프로그래밍",
+  CAREER: "취업/커리어",
+  QUALIFICATION_EXAM: "고시/공무원",
+  HOBBY: "취미",
+  OTHERS: "기타"
+};
