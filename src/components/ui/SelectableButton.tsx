55011545622408c12c770a359412aaa4a9e0d97a
hershyYJ <juj0478@naver.com>
2025-03-01 17:32:29 +0900

    [feature/#17] Create selectable button component

---
+++ src/components/ui/SelectableButton.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+interface SelectableButtonProps {
+  label: string;
+  isSelected: boolean;
+  onClick: () => void;
+  disabled?: boolean;
+}
+
+const SelectableButton: React.FC<SelectableButtonProps> = ({ label, isSelected, onClick, disabled }) => {
+  const buttonStyles = {
+    padding: '10px 20px',
+    margin: '5px',
+    border: '1px solid #ccc',
+    borderRadius: '5px',
+    cursor: disabled ? 'not-allowed' : 'pointer',
+    backgroundColor: disabled ? 'white' : isSelected ? 'rgb(242, 249, 255)' : 'white',
+    color: '#1f72c5',
+    opacity: disabled ? 0.5 : 1,
+  };
+
+  return (
+    <button
+      style={buttonStyles}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default SelectableButton;
