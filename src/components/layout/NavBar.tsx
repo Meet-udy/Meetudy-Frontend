54b54089dcf4ed7ee25965e91607b7ac49a40c36
hershyYJ <juj0478@naver.com>
2025-03-18 16:39:24 +0900

    [feature/#21] Add navigation to study group listing page on Group button click

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 import "./NavBar.css";
 
 export const NavBar: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleGroupClick = () => {
+    navigate('/study-groups'); 
+  };
+
   return (
     <nav className="nav-menu">
       <ul>
-        <li>GROUP</li>
+        <li onClick={handleGroupClick}>GROUP</li>
         <li>PLANNER</li>
         <li>COMMUNITY</li>
         <li>MYPAGE</li>
