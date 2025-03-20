f13123f22f1045c1e58009989b5b571aeee6c3a3
hershyYJ <juj0478@naver.com>
2025-03-20 16:05:24 +0900

    [feature/#23] Connect Study Group Detail Retrieval API and Study Group Creation API

--- src/api/studyGroupApi.ts
+++ src/api/studyGroupApi.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
-import { transformStudyGroupData } from "../utils/studyGroupUtils.ts";
+import { transformStudyGroupData, transformStudyGroupFromResult } from "../utils/studyGroupUtils.ts";
 
 const API_BASE_URL = "http://localhost:8080"; 
 
 export interface StudyGroupDto {
+  id: number
   category: string;
   name: string;
   description: string;
@@ -23,7 +24,8 @@ export interface ApiResponse<T> {
 
 export const getSortedStudyGroups = async (accessToken: string, sortBy: string) => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/search/sort?sortBy=${sortBy}`, {
+    const response = await axios.get(
+      `${API_BASE_URL}/search/sort?sortBy=${sortBy}`, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
@@ -35,9 +37,40 @@ export const getSortedStudyGroups = async (accessToken: string, sortBy: string) 
   }
 };
 
+export const getStudyGroupById = async (groupId: number) => {
+  try {
+    const response = await axios.get(
+      `${API_BASE_URL}/study-groups/${groupId}`,
+    );
+
+    return transformStudyGroupFromResult(response.data.result);
+  } catch(error) {
+    throw new Error("스터디 그룹을 조회할 수 없습니다.");
+  }
+};
+
+export const createStudyGroup = async (accessToken: string, studyGroupDto: StudyGroupDto) => {
+  try {
+    const response = await axios.post<ApiResponse<string>>(
+      `${API_BASE_URL}/study-groups`,
+      studyGroupDto,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        },
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    throw new Error("스터디 그룹 생성에 실패했습니다.");
+  }
+};
+
 export const getCreatedStudyGroups = async (accessToken: string) => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/study-groups/created-groups`, {
+    const response = await axios.get(
+      `${API_BASE_URL}/study-groups/created-groups`, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
@@ -51,7 +84,8 @@ export const getCreatedStudyGroups = async (accessToken: string) => {
 
 export const getJoinedStudyGroups = async (accessToken: string) => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/study-groups/joined-groups`, {
+    const response = await axios.get(
+      `${API_BASE_URL}/study-groups/joined-groups`, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
