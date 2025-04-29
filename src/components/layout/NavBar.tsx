b3b4b2089adddc39325ee46e084796a66c314479
hershyYJ <127376237+hershyYJ@users.noreply.github.com>
2025-04-29 17:24:50 +0900

    [feature/#35] Add chat button and navigation to chat rooms page in NavBar

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -9,11 +9,15 @@ export const NavBar: React.FC = () => {
     navigate('/study-groups'); 
   };
 
+  const handleChatClick = () => {
+    navigate('/chat-rooms');
+  }
+
   return (
     <nav className="nav-menu">
       <ul>
         <li onClick={handleGroupClick}>GROUP</li>
-        <li>PLANNER</li>
+        <li onClick={handleChatClick}>CHAT</li>
         <li>COMMUNITY</li>
         <li>MYPAGE</li>
         <li>NOTICE</li>
