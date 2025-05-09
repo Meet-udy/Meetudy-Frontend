67bafd5762e2dff30909d06afbc3d5f2f9b46c03
hershyYJ <127376237+hershyYJ@users.noreply.github.com>
2025-05-09 15:55:42 +0900

    [feature/#39] Add FontAwesome configuration and disable automatic CSS injection

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
+import '@fortawesome/fontawesome-svg-core/styles.css'; // 꼭 필요
+import { config } from '@fortawesome/fontawesome-svg-core'; 
+config.autoAddCss = false; //
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
